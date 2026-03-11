259504d681b0e5f6d4fe6d526ce34611dc524e19
nayrneerg <ryan@thisiscontroltower.com>
2026-03-10 17:18:09 -0700

    feat: add config files and types

--- babel.config.js
+++ babel.config.js
@@ -4,19 +4,6 @@ module.exports = function(api) {
     presets: ['babel-preset-expo'],
     plugins: [
       'expo-router/babel',
-      ['module-resolver', {
-        root: ['./'],
-        alias: {
-          '@': './',
-          '@components': './components',
-          '@hooks': './hooks',
-          '@lib': './lib',
-          '@stores': './stores',
-          '@types': './types',
-          '@constants': './constants',
-        },
-      }],
-      'react-native-paper/babel',
       'react-native-reanimated/plugin',
     ],
   };
