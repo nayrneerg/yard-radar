fa37c03edd762aef5467c50641488b98c139de80
nayrneerg <ryan@thisiscontroltower.com>
2026-03-10 17:09:50 -0700

    feat: add sales store

---
+++ stores/salesStore.ts
@@ -0,0 +1,22 @@
+import { create } from 'zustand';
+import { Sale, SaleFilters, SaleCategory } from '@/types';
+
+interface SalesState {
+  nearbySales: Sale[]; selectedSale: Sale | null; filters: SaleFilters;
+  userLocation: { latitude: number; longitude: number; } | null;
+  setNearbySales: (sales: Sale[]) => void; setSelectedSale: (sale: Sale | null) => void;
+  setFilters: (filters: Partial<SaleFilters>) => void;
+  setUserLocation: (location: { latitude: number; longitude: number } | null) => void;
+  resetFilters: () => void;
+}
+
+const defaultFilters: SaleFilters = { categories: [], maxDistance: 10, dateRange: {} };
+
+export const useSalesStore = create<SalesState>((set) => ({
+  nearbySales: [], selectedSale: null, filters: defaultFilters, userLocation: null,
+  setNearbySales: (nearbySales) => set({ nearbySales }),
+  setSelectedSale: (selectedSale) => set({ selectedSale }),
+  setFilters: (filters) => set((state) => ({ filters: { ...state.filters, ...filters } })),
+  setUserLocation: (userLocation) => set({ userLocation }),
+  resetFilters: () => set({ filters: defaultFilters }),
+}));
